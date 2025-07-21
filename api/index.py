c3d4030ab3a40698eaafe7f6460a7b5ce4d7e7a0
fameminty <omarshekhuna0@gmail.com>
2025-07-21 11:35:33 -0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,52 @@
-@app.route("/api/CheckForBadName", methods=["POST", "GET"])
-def checkforbadname():
-    rjson = request.get_json() 
-    function_result = rjson["FunctionArgument"]
-    playfab_id = rjson["CallerEntityProfile"]["Lineage"]["MasterPlayerAccountId"]
-    name = function_result["name"].upper()
-    forRoom = function_result["forRoom"]
+const BadNames = {
+    // name : hours banned for
+    // 0 hours = perm
+    "_": 0, // cheating no name
+    "NIG": 24, // obvious reasons
+};
 
-    if forRoom == True:
-        return jsonify({"result": 0})
+const AllowedNames = [
+    "MINIGAMES" // contains NIG so make it a allowed name
+];
+
+handlers.CheckForBadName = function(args, context) {
+    const Name = args.name;
+    const ForRoom = args.forRoom; // is for room (false if for troop/name)
+
+    for (let badName in BadNames) {
+        if (Name.includes(badName) && !AllowedNames.includes(Name)) {
+            if (BadNames[badName] == 0) {
+                server.BanUsers({
+                    Bans: [{
+                        PlayFabId: currentPlayerId,
+                        DurationInHours: BadNames[badName],
+                        Reason: "RULE BREAKING NAME: " + Name.toUpperCase() + ". PERMANENT BAN. USE YOUR HEAD NEXT GAME YOU PLAY."
+                    }]
+                });
+
+                return {
+                    "result": 2,
+                    "banLength": 0
+                };
+            }
+
+            server.BanUsers({
+                Bans: [{
+                    PlayFabId: currentPlayerId,
+                    DurationInHours: BadNames[badName],
+                    Reason: "RULE BREAKING NAME: " + Name.toUpperCase() + ". " + BadNames[badName] + " HOUR BAN. USE YOUR HEAD NEXT TIME."
+                }]
+            });
+
+            return {
+                "result": 2,
+                "banLength": BadNames[badName]
+            };
+        }
+    }
+
+    return {
+        "result": 0,
+        "banLength": -1
+    };
+};
